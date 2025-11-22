3ee56cbd79931e2362220ff7a5c0082111fe4ed4
Tommy-Tomato <156547507+Tommy-Tomato@users.noreply.github.com>
2025-11-22 18:19:00 -0500

    many updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        epoch: ["Epoch", "sans-serif"],
+      },
+    },
+  },
   plugins: [require("daisyui")],
 };
